adfc5551af3e685a705795d5dcaef5b2d412ad41
PEDROBELTRAN001 <145783001+PEDROBELTRAN001@users.noreply.github.com>
2026-02-24 18:37:18 -0400

    ultima subida

--- src/app/editor-filtro/editor-filtro.ts
+++ src/app/editor-filtro/editor-filtro.ts
@@ -10,14 +10,26 @@ export class EditorFiltroComponent {
   brillo = signal(100);
   contraste = signal(100);
   blur = signal(0);
+  sepia = signal(0);
+  saturacion = signal(100);
+  hueRotate = signal(0);
+  escalaDeGrises = signal(0);
+  invertir = signal(0);
+  opacidad = signal(100);
 
   filtroScss = computed(() => {
-    return `brightness(${this.brillo()}%) contrast(${this.contraste()}%) blur(${this.blur()}px)`;
+    return `brightness(${this.brillo()}%) contrast(${this.contraste()}%) blur(${this.blur()}px) sepia(${this.sepia()}%) saturate(${this.saturacion()}%) hue-rotate(${this.hueRotate()}deg) grayscale(${this.escalaDeGrises()}%) invert(${this.invertir()}%) opacity(${this.opacidad()}%)`;
   })
   actualizar(prop: string, evento: Event) {
     const valor = (evento.target as HTMLInputElement).value;
     if (prop === 'brillo') this.brillo.set(+valor);
     if (prop === 'contraste') this.contraste.set(+valor);
     if (prop === 'blur') this.blur.set(+valor);
+    if (prop === 'sepia') this.sepia.set(+valor);
+    if (prop === 'saturacion') this.saturacion.set(+valor);
+    if (prop === 'hueRotate') this.hueRotate.set(+valor);
+    if (prop === 'escalaDeGrises') this.escalaDeGrises.set(+valor);
+    if (prop === 'invertir') this.invertir.set(+valor);
+    if (prop === 'opacidad') this.opacidad.set(+valor);
   }
 }
