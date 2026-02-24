5952ced423027612952c710d25d5d6797a3677e2
PEDROBELTRAN001 <145783001+PEDROBELTRAN001@users.noreply.github.com>
2026-02-24 17:32:36 -0400

    subir todo

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EditorFiltroComponent } from './editor-filtro/editor-filtro';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [EditorFiltroComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('mi-proyecto-grafico');
+
 }
